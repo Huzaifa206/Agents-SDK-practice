70fa88ceb47ee77b02d83f1e0d130e77b9ea2b61
Huzaifa Ahmed Siddiqui <huzaifaasiddiqui@gmail.com>
2025-08-09 23:00:46 +0500

    prompting.py

--- prompting.py
+++ prompting.py
@@ -1,4 +1,5 @@
-improve my instructions and descriptions according to gpt 5 prompting guide
+perplexity
+Improve my instructions and descriptions according to gpt 5 prompting guide
 "https://cookbook.openai.com/examples/gpt-5/gpt-5_prompting_guide"
 
 Do Not change any code.
